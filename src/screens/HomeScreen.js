3b6c075c55aa726d215a83b5e62c1ad27fa8a93c
viditviionn <vidit.viionn@gmail.com>
2024-09-08 22:07:11 +0530

    added dummydata to constant

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -4,18 +4,7 @@ import styles from '../styles/HomeScreenStyles';
 import ClickbaitCard from '../components/ClickbaitCard';
 import CategoryCard from '../components/CategoryCard';
 import SearchBar from '../components/SearchInput';
-
-const clickbaitData = [
-  { id: '1', title: 'This is a title and more text', image: 'https://picsum.photos/200/300' },
-  { id: '2', title: 'This is a title and more text', image: 'https://picsum.photos/200/301' },
-  { id: '3', title: 'This is a title and more text', image: 'https://picsum.photos/200/302' },
-];
-
-const categoryData = [
-  { id: '1', title: 'EXERCISES', subtitle: '132 Exercises', color: '#8B4513', image: 'https://picsum.photos/200/303' },
-  { id: '2', title: 'RECIPES', subtitle: '52 Entries', color: '#FF69B4', image: 'https://picsum.photos/200/304' },
-  { id: '3', title: 'NUTRITIONAL', subtitle: '', color: '#FFA500', image: 'https://picsum.photos/200/305' },
-];
+import { categoryData, clickbaitData } from '../utils/constants';
 
 const HomeScreen = () => {
   const handleCategoryPress = (category) => {
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -11,3 +11,16 @@ export const colors = {
     searchBarBorder: '#ddd',
     white: 'white'
 }
+
+export const clickbaitData = [
+    { id: '1', title: 'This is a title and more text', image: 'https://picsum.photos/200/300' },
+    { id: '2', title: 'This is a title and more text', image: 'https://picsum.photos/200/301' },
+    { id: '3', title: 'This is a title and more text', image: 'https://picsum.photos/200/302' },
+  ];
+  
+export const categoryData = [
+    { id: '1', title: 'EXERCISES', subtitle: '132 Exercises', color: '#8B4513', image: 'https://picsum.photos/200/303' },
+    { id: '2', title: 'RECIPES', subtitle: '52 Entries', color: '#FF69B4', image: 'https://picsum.photos/200/304' },
+    { id: '3', title: 'NUTRITIONAL', subtitle: '', color: '#FFA500', image: 'https://picsum.photos/200/305' },
+  ];
+  
